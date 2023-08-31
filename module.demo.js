37ac44ccd6ba814df86b58096a2fff477a0b7fae
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-08-31 21:51:30 +0300

    update modules

--- module.demo.js
+++ module.demo.js
@@ -1,3 +1,4 @@
 // This module will use my-module.js
 const myModule = require("./my-module");
 console.log(myModule);
+console.log(myModule.myText);
--- my-module.js
+++ my-module.js
@@ -1,1 +1,3 @@
-exports.myText = "Hello, stranger. You gotta do what you gotta do to survive here.";
+exports.myText =
+  "Hello, stranger. You gotta do what you gotta do to survive here.";
+exports.sayHello = "You wanna hang out with me?";
